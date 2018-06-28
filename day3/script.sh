6b439136a9a81386421dca5a7a446c7ca1c7e896
lovedeepsh <lovedeeps789@gmail.com>
2018-06-28 22:49:04 +0530

    day3 updated

--- day3/script.sh
+++ day3/script.sh
@@ -5,6 +5,7 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 git clone https://github.com/lovedeepsh/AWS.git
+sudo su
 cd AWS/day3
 sudo ansible-playbook main.yml
  
