9c264c94c7abd4e6b68d2428d265daac7266064f
lovedeepsh <lovedeeps789@gmail.com>
2018-06-28 19:01:10 +0530

    day3 role and script added

--- day3/script.sh
+++ day3/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install git -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
 sudo apt-get install ansible -y
-git clone 
+git clone https://github.com/lovedeepsh/AWS.git
+cd AWS/day3
+sudo ansible-playbook main.yml
+ 
 
